2890045e9b9665efa76afc8009290e710efe20fa
Daniel Roman Caro <danroman@misena.edu.co>
2022-02-02 18:40:07 -0500

    Cambiando ando

--- homeless.py
+++ homeless.py
@@ -10,3 +10,9 @@ class StandarHomeless(Homeless):
 
     def walk(self):
         return random.choice([(1,0),(-1,0),(0,1),(0,-1)])
+
+"""Super es para heredar de la clase padre"""
+
+
+
+"""el random.choice tiene 2 argumentos 1,0. pero puede tener mas de estos como unos 5 o 6 dimensiones o datos"""
