abdd22ef446393635dcb88240e79671a1e09e699
Daniel Roman Caro <danroman@misena.edu.co>
2022-02-09 19:02:58 -0500

    change

--- random_road.py
+++ random_road.py
@@ -29,7 +29,7 @@ def walking(homeless,steps,type_homeless):
 
     return homeless.distance_origin()
 
-def simulate_walk(steps,number_of_attemps,type_of_attemps):
+def simulate_walk(steps,number_of_attemps,type_homeless):
     homeless =[]
     distance = []
 
@@ -44,28 +44,25 @@ def graph(x_graph,y_graph,know,steps):
     paint.line(x_graph,y_graph,legend_label = str(steps) + "Pasos")
     final_x = x_graph[-1]
     final_y = y_graph[-1]
-    paint.diamond_cross(0,0,fill_color = "blue",line_color = "blue" size = 18)
-    paint.diamond_cross(final_x,final_y,fill_color = "red",line_color = "red" size = 18)
+    paint.diamond_cross(0,0,fill_color = "blue", line_color = "blue", size = 18)
+    paint.diamond_cross(final_x,final_y,fill_color = "red",line_color = "red", size = 18)
     final_stretch_x = [0,final_x]
     final_stretch_y = [0,final_y]
-    graph.line(final_stretch_x,final_stretch_y,line_width = 2,color = "red")
+    paint.line(final_stretch_x,final_stretch_y,line_width = 2,color = "red")
     show(paint)
 
 def main(walk_distance,number_of_attemps,type_homeless):
-    average_walking_distance = []
     for steps in walk_distance:
         distance = simulate_walk(steps,number_of_attemps,type_homeless)
         distance_average =round(sum(distance)/len(distance))
         distance_max = max(distance)
         distance_min = min(distance)
-        average_walking_distance.append(distance_average)
         print(f"{type_homeless.__name__}caminata aleatoria")
         print(f"Media = {distance_average}")
         print(f"Max = {distance_max}")
         print(f"Min = {distance_min}")
-        graph(walk_distance,average_walking_distance)
 
 if __name__ == "__main__":
-    walk_distance = [10,100,1000,10000]
-    number_of_attemps = 100
+    walk_distance = [100]
+    number_of_attemps = 3
     main(walk_distance,number_of_attemps,StandarHomeless)
